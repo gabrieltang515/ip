0d0ae5720722293e04ddfd3fc317db68b6a4e046
gabrieltang515 <gabrieltang515@gmail.com>
2025-08-20 20:17:59 +0800

    Atlas.java: Refactoring main function

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Atlas {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,9 +7,28 @@ public class Atlas {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String LINE =
+                "____________________________________________________________";
+
+        System.out.println(LINE);
         System.out.println("Hello I'm Atlas!");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println(" " + input);   // echo
+            System.out.println(LINE);
+        }
+        in.close();
     }
 }
 
